cd7748d1e405853e41c3efa0f3233b79367572f4
Md Naim Hossen <134233716+naiem9009@users.noreply.github.com>
2024-01-02 13:54:04 +0600

    Update TopPlay.jsx

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -61,7 +61,7 @@ const TopPlay = () => {
   }
   
   return (
-    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w[400px] flex-col">
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 w[400px] flex-col">
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
           <h2 className="text-white font-bold text-2xl">Top Charts</h2>
